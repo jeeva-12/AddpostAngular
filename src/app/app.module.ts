ee60a922c9e5ab6b0091f55ab8e50be714a31ba9
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-25 15:40:27 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewComponent } from './view/view.component';
+import { AddpostComponent } from './addpost/addpost.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewComponent
+    ViewComponent,
+    AddpostComponent
   ],
   imports: [
     BrowserModule,
